18b6e2309cbed4b5f79b5f8ba05fe1797591742e
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-07-17 14:10:51 +0200

    Add Starter files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Search from './components/search/Search';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
