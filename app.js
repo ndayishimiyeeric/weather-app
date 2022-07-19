68af92b354786fa550957e3a52e6afe1634c758c
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-07-19 08:45:15 +0200

    deploy

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
+  app.use(express.static(path.join(__dirname, './build')));
   app.get('*', (req, res) => {
-    req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './', 'build', 'index.html'));
   });
+} else {
+  app.get('/', (req, res) => res.send('Please use the production server'));
 }
 
 app.list(port, (err) => {
